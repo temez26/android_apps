084d0696c91da8e201a45e63eb0f0aabad2a8810
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2025-08-15 13:26:15 +0300

    started making dessert app that stored the data to phones local storage datastore

--- dessert/app/src/main/java/com/example/dessertrelease/data/UserPreferenceRepository.kt
+++ dessert/app/src/main/java/com/example/dessertrelease/data/UserPreferenceRepository.kt
@@ -1,4 +1,41 @@
 package com.example.dessertrelease.data
 
-class UserPreferenceRepository {
+import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.emptyPreferences
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+import java.util.prefs.Preferences
+
+class UserPreferenceRepository(private val dataStore: DataStore<Preferences>) {
+
+    val isLinearLayout: Flow<Boolean> = dataStore.data
+        .catch {
+            if(it is IOException) {
+                Log.e(TAG, "Error reading preferences.", it)
+                emit(emptyPreferences())
+            } else {
+                throw it
+            }
+        }
+        .map { preferences ->
+            preferences[IS_LINEAR_LAYOUT] ?: true
+        }
+
+
+
+    private companion object {
+        val IS_LINEAR_LAYOUT = booleanPreferencesKey("is_linear_layout")
+        const val TAG = "UserPreferencesRepo"
+    }
+
+    suspend fun saveLayoutPreference(isLinearLayout: Boolean) {
+        dataStore.edit {
+            preferences[IS_LINEAR_LAYOUT] = isLinearLayout
+
+        }
+    }
 }
